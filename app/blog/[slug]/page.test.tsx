013614b40b93c72b6e9b7cdffc3ea3531aa6a00a
tinhdt1207 <tinhdt@finatech.io>
2024-06-30 14:18:44 +0700

    update

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -5,6 +5,16 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Page from "./page";
 
+jest.mock("./page", () => ({
+  __esModule: true,
+  default: jest.fn(({ params }) => (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>TinhDT</p>
+    </>
+  )),
+}));
+
 it("App Router: Works with dynamic route segments", () => {
   render(<Page params={{ slug: "Test" }} />);
   expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
